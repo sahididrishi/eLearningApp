dee7ae04aaa2434f67e7237294e6854d3e8415c0
pega <pegasisprime133@icloud.com>
2025-01-12 03:50:41 +0530

    project setup

--- elearning/elearning/settings.py
+++ elearning/elearning/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd-party
+    'rest_framework',
+    'channels',
+
+
+    #my apps
+    'users',
+    'courses',
+    'chat'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +64,7 @@ ROOT_URLCONF = 'elearning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],    ## global templates dir
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +78,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'elearning.wsgi.application'
-
+ASGI_APPLICATION = 'elearning_project.asgi.application'   # For Channels
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -112,10 +122,25 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+
+#     AUTH_USER_MODEL = 'users.CustomUser'  # Custom user model
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
